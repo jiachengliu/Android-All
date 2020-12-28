9265590e3f0157974a4a513ab6bf1432a5297d78
LiuJiacheng <liujiacheng@mylike.local>
2020-12-28 11:06:33 +0800

    1.修改异步请求回调数据bug

--- lib_http/src/main/java/com/mylike/http/IResponseHandler.java
+++ lib_http/src/main/java/com/mylike/http/IResponseHandler.java
@@ -1,5 +1,8 @@
 package com.mylike.http;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import java.io.IOException;
 
 /**
@@ -8,10 +11,28 @@ import java.io.IOException;
  * desc:
  */
 public interface IResponseHandler {
-    void handlerSuccess(Callback callback,Response response);
+    /**
+     * 线程切换,http请求成功时的回调
+     *
+     * @param callback &emsp;回调接口
+     * @param response &emsp;返回结果
+     */
+    void handlerSuccess(Callback callback, Response response);
+
+    /**
+     * 线程切换,http请求失败时候的回调
+     *
+     * @param callback &emsp;回调接口
+     * @param request  &emsp;请求
+     * @param e        &emsp;可能产生的异常
+     */
     void handFail(Callback callback, Request request, IOException e);
 
+
     IResponseHandler RESPONSE_HANDLER = new IResponseHandler() {
+
+        Handler HANDLER = new Handler(Looper.getMainLooper());
+
         @Override
         public void handlerSuccess(final Callback callback, final Response response) {
             Runnable runnable = new Runnable() {
@@ -25,21 +46,31 @@ public interface IResponseHandler {
 
         @Override
         public void handFail(final Callback callback, final Request request, final IOException e) {
-
             Runnable runnable = new Runnable() {
                 @Override
                 public void run() {
-                    callback.onFail(request,e);
+                    callback.onFail(request, e);
                 }
             };
             execute(runnable);
         }
-        public void removeAllMessage(){
 
-        }
-        private void execute(Runnable runnable){
 
+        /**
+         * 移除所有消息
+         */
+        public void removeAllMessage() {
+            HANDLER.removeCallbacksAndMessages(null);
         }
+
+        /**
+         * 线程切换
+         * @param runnable
+         */
+        private void execute(Runnable runnable) {
+            HANDLER.post(runnable);
+        }
+
     };
 
 }
